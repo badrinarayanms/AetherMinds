4807d4caf1b40644b8597cda244543bd0796a07f
badrinarayanms <badrinarayan.moola@gmail.com>
2024-09-07 21:08:13 +0530

    test

--- app/(main)/grades/page.js
+++ app/(main)/grades/page.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
-export const page = () => {
+const page = () => {
   return (
-    <div>page</div>
+    <div className="w-full h-full p-10 m-0">
+            <div className="">
+                <h1 className='text-5xl text-[#00517A] font-gsans font-bold'>Grade</h1>
+            </div>
+    </div>
   )
 }
+
+export default page
