615ace48761a4a18f5da75baaf4b412d888f7c7a
badrinarayanms <badrinarayan.moola@gmail.com>
2024-09-07 21:06:14 +0530

    test

--- app/(main)/performancereport/page.js
+++ app/(main)/performancereport/page.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className="w-full h-full p-10 m-0">
+            <div className="">
+                <h1 className='text-5xl text-[#00517A] font-gsans font-bold'>Performance Report</h1>
+            </div>
+    </div>
   )
 }
 
